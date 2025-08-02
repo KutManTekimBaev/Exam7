3c9e1ffed599663007647f100751d9f1e8f3d435
Kutman Tekimbaev <kutik9555@gmail.com>
2025-08-02 13:49:45 +0600

    Добавил необходимые зависимости validation liquibase h2database jdbc security прописал примерную структуру проекта прописал миграцию для создания бд

--- src/main/java/org/example/exam7/Exam7Application.java
+++ src/main/java/org/example/exam7/Exam7Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Exam7Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Exam7Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Exam7Application.class, args);
+    }
 
 }

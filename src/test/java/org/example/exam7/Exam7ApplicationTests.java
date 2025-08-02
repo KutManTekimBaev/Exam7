0ad4a78915e4d6b3939316269525cb5dd6128004
Kutman Tekimbaev <kutik9555@gmail.com>
2025-08-02 12:30:04 +0600

    first commit

---
+++ src/test/java/org/example/exam7/Exam7ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.exam7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exam7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
